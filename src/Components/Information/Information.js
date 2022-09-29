a55207d85b72d9db05b336201b11dc9b8ec57b4f
abdullah al emon <abdullahemon905@gmail.com>
2022-09-29 21:50:50 +0600

    second commit

--- src/Components/Information/Information.js
+++ src/Components/Information/Information.js
@@ -7,8 +7,8 @@ const Information = () => {
     return (
         <div>
             <div className='info'>
-                <div >
-                <img src='/images/pic.jpg' alt="" />
+                <div className='mr-1'>
+                    <img src='/images/pic.jpg' alt="" />
                 </div>
                 <div>
                     <h3 className='font-semibold'>Zahid Hossain</h3>
@@ -16,18 +16,57 @@ const Information = () => {
                 </div>
             </div>
             <div className='flex bg-blue-50 mx-3 justify-between px-4 py-2 rounded-[15px]'>
-            <div>
-                <p className='text-[20px] font-[600]'>75<small className='font-[400] text-stone-400'>Kg</small></p>
-                <p className='font-[400] text-zinc-500'>Weight</p>
+                <div>
+                    <p className='text-[20px] font-[600]'>75<small className='font-[400] text-stone-400'>Kg</small></p>
+                    <p className='font-[400] text-zinc-500'>Weight</p>
+                </div>
+                <div>
+                    <p className='text-[20px] font-[600]'>6.5</p>
+                    <p className='font-[400] text-zinc-500'>Height</p>
+                </div>
+                <div>
+                    <p className='text-[20px] font-[600]'>25<small className='font-[400] text-stone-400'>Yrs</small></p>
+                    <p className='font-[400] text-zinc-500'>Age</p>
+                </div>
             </div>
-            <div>
-                <p className='text-[20px] font-[600]'>6.5</p>
-                <p className='font-[400] text-zinc-500'>Height</p>
+            <div className="break">
+                <h3 className='font-semibold ml-3 mb-4'>Add A Break</h3>
+                <div className='flex bg-blue-50 mx-3 justify-between px-2 py-2 rounded-[15px]'>
+                    <div>
+                        <p className='text-[20px] font-[600] bg-white px-1 py-1 rounded-[20px] mx-2'>10s</p>
+                    </div>
+                    <div>
+                        <p className='text-[20px] font-[600] bg-white px-1 py-1 rounded-[20px] mx-2'>20s</p>
+                    </div>
+                    <div>
+                        <p className='text-[20px] font-[600] bg-white px-1 py-1 rounded-[20px] mx-2'>30s</p>
+                    </div>
+                    <div>
+                        <p className='text-[20px] font-[600] bg-white px-1 py-1 rounded-[20px] mx-2'>40s</p>
+                    </div>
+                </div>
             </div>
-            <div>
-                <p className='text-[20px] font-[600]'>25<small className='font-[400] text-stone-400'>Yrs</small></p>
-                <p className='font-[400] text-zinc-500'>Age</p>
+            <div className="break">
+                <h3 className='font-semibold ml-3 mb-4'>Exercise Details</h3>
+                <div className='flex bg-blue-50 mx-3 mb-4 justify-between px-4 py-2 rounded-[15px]'>
+                    <div>
+                        <p className='text-[17px] font-[600]'>Exercise time</p>
+                    </div>
+                    <div>
+                        <p className='font-[400] text-zinc-500'>0s</p>
+                    </div>
+                </div>
+                <div className='flex bg-blue-50 mx-3 justify-between px-4 py-2 rounded-[15px]'>
+                    <div>
+                        <p className='text-[17px] font-[600]'>Break time</p>
+                    </div>
+                    <div>
+                        <p className='font-[400] text-zinc-500'>0s</p>
+                    </div>
+                </div>
             </div>
+            <div className="break">
+            <button className='btn-info bg-blue-500 hover:bg-blue-400'>Added to List</button>
             </div>
         </div>
     );
